2c3da129d039bfd4f67196b314f42e9bcb7874d1
GiangTPham <phatr2000@gmail.com>
2021-07-17 15:02:04 +0700

    add a lot

--- app.py
+++ app.py
@@ -3,6 +3,12 @@ from message import *
 import os
 import pandas as pd
 import numpy as np  
+from demo import Demo
+import torch
+import cv2
+from experiment import Structure, Experiment
+from concern.config import Configurable, Config
+import math
 
 app = Flask(__name__)
 app.secret_key = os.urandom(12).hex()
@@ -23,7 +29,17 @@ def get_prediction():
     file = request.files['file']
     if file and file.filename != '' and allowed_file(file.filename):
       img_bytes = file.read()
-      ann = pd.read_csv('test/LienPhai-2484.txt',header=None).to_numpy().astype(np.float32)
+      args = {'exp': 'experiments/seg_detector/td500_resnet18_deform_thre.yaml', 
+              'resume': 'weights/final', 'image_path': 'test/' + file.filename, 
+              'result_dir': './demo_results/', 'image_short_side': 736, 'box_thresh': 0.6, 'visualize': False, 
+              'resize': False, 'polygon': False, 'eager_show': False}
+      conf = Config()
+      experiment_args = conf.compile(conf.load(args['exp']))['Experiment']
+      experiment_args.update(cmd=args)
+      experiment = Configurable.construct_class_from_config(experiment_args)
+      ann = Demo(experiment, experiment_args, cmd=args).inference(args['image_path'], args['visualize'])
+      ann.reshape((ann.shape[0], -1))
+      print(ann.shape)
       ann = (ann*500/3563).astype(np.int16)[..., :8].tolist()
       return success(ann)
     return error('[Error]: Unsupported file extension!')
